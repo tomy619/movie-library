517c874eacc12170ff921d568746ba6422416fcc
Tomy Mathew <tomym619@gmail.com>
2019-07-14 15:56:03 +0530

    Updates

---
+++ precache-manifest.b743d750f66c55d7b0ac920a60f02ea0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894c0e8959ec467ffa1d90c979e3e501",
+    "url": "/movie-library/index.html"
+  },
+  {
+    "revision": "70b23e85022b6ddebe72",
+    "url": "/movie-library/static/css/main.4550f0cd.chunk.css"
+  },
+  {
+    "revision": "894d98e5d5ad5a20993c",
+    "url": "/movie-library/static/js/2.29da93e9.chunk.js"
+  },
+  {
+    "revision": "70b23e85022b6ddebe72",
+    "url": "/movie-library/static/js/main.e2a28247.chunk.js"
+  },
+  {
+    "revision": "5eb29b8470da9d2d063b",
+    "url": "/movie-library/static/js/runtime~main.a52aa90b.js"
+  }
+]);
