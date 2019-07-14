c99435c77a6675927f4d7de0d68f1143d5820d0a
Tomy Mathew <tomym619@gmail.com>
2019-07-14 16:51:06 +0530

    ui correction

--- src/components/MediaLibrary/components/body/image.js
+++ src/components/MediaLibrary/components/body/image.js
@@ -1,16 +1,41 @@
-import React , { useState } from 'react';
+import React , { useState, useEffect, useLayoutEffect } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 import LazyLoad from 'react-lazy-load';
 
 function MyImage(props) {
 
-
   const [showLowResImages, showLowResImagesHandler] = useState(true)
+  const [height, heighHandler] = useState(180)
+  const [width, widthHandler] = useState(120)
 
   const imagedLoaded = () => {
     showLowResImagesHandler(false)
   }
+
+  useEffect(()=>{
+    console.log(window.innerWidth);
+    if(window.innerWidth > 808){
+      heighHandler(300);
+      widthHandler(200)
+    }
+  })
+
+  useLayoutEffect(()=>{
+    console.log(window.innerWidth);
+    if(window.innerWidth > 808){
+      heighHandler(300);
+      widthHandler(200)
+    }
+    else if( 820 >  window.innerWidth < 500){
+      heighHandler(280);
+      widthHandler(190)
+    }
+    else{
+      heighHandler(180);
+      widthHandler(120)
+    }
+  })
 
 
   return (
@@ -19,15 +44,14 @@ function MyImage(props) {
       <LazyLoadImage
         alt={props.src}
         effect="blur"
-        height={180}
-        width={120}
+        height={height}
+        width={width}
         key={props.src}
         afterLoad={imagedLoaded}
         placeholderSrc={showLowResImages ? props.alt : props.src}
         // scrollPosition={scrollPosition}
         src={props.src}
         threshold={200}
-        wrapperClassName="gallery-img-wrapper" 
         />
 
       {/* <LazyLoad
